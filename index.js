6d0148ddadaa7018628f6ddf5c859c42e49781f6
Issa Abubakry Olamilekan <issaabubakry@gmail.com>
2024-05-15 14:33:12 -0700

    query selected the container and weather details section

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const 
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+
+
+search.addEventListener('click', () => {
+
+    const APIKey = ''
+
+});
