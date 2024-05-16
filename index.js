11f0f0571f9d0f3f24fd137c7f4891da4c4bf668
Issa Abubakry Olamilekan <issaabubakry@gmail.com>
2024-05-15 22:18:02 -0700

    make use of a switch case and added a new image

--- index.js
+++ index.js
@@ -15,8 +15,21 @@ search.addEventListener('click', () => {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}').then((response) => response.json()).then(json => {
 
         const image = document.querySelector('.weather-box img');
-        const image = document.querySelector('.weather-box img');
-        const image = document.querySelector('.weather-box img');
+        const temperature = document.querySelector('.weather-box .temperature');
+        const description = document.querySelector('.weather-box .description');
+        const humidity = document.querySelector('.weather-details .humidity span');
+        const wind = document.querySelector('.weather-details .wind span');
+
+
+        switch (json.weather[0].main) {
+            case 'Clear':
+                img.src = 'img/clear.png';
+                
+                break;
+        
+            default:
+                break;
+        }
 
     });
 
