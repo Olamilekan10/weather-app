4fb9655c52f03e13c3529ef5bcf24df552cd0fbc
Issa Abubakry Olamilekan <issaabubakry@gmail.com>
2024-05-16 14:58:51 -0700

    set a condition to remove weather box if 404 occurs

--- index.js
+++ index.js
@@ -15,6 +15,21 @@ search.addEventListener('click', () => {
     
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then((response) => response.json()).then(json => {
 
+        if(json.cod === '404') {
+            container.style.height = '400px';
+            weatherBox.classList.remove('active');
+            weatherDetails.classList.remove('active');
+            error404.classList.add('active');
+            return;
+        }
+
+
+        // container.style.height = '400px';
+        // weatherBox.classList.remove('active');
+        // weatherDetails.classList.remove('active');
+        // error404.classList.add('active');
+
+
         const image = document.querySelector('.weather-box img');
         const temperature = document.querySelector('.weather-box .temperature');
         const description = document.querySelector('.weather-box .description');
