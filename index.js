8a24d5baa6a464e4ff3ff593ee83f3e9ceba05dd
Issa Abubakry Olamilekan <issaabubakry@gmail.com>
2024-05-15 22:02:52 -0700

    fetch the weather api and return a json response

--- index.js
+++ index.js
@@ -7,5 +7,17 @@ const weatherDetails = document.querySelector('.weather-details');
 search.addEventListener('click', () => {
 
     const APIKey = 'ea837899c3e78816fa642dd545ce38ff';
+    const city = document.querySelector('.search-box input').value;
+
+    if (city == '') 
+        return;
+    
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}').then((response) => response.json()).then(json => {
+
+        const image = document.querySelector('.weather-box img');
+        const image = document.querySelector('.weather-box img');
+        const image = document.querySelector('.weather-box img');
+
+    });
 
 });
